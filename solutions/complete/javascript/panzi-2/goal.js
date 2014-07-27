5dc840026cb41e34f4252ebd342eeae8a00a958c
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2014-07-27 21:44:45 +0200

    shaved off 12 characters with => syntax as suggested by /u/inmatarian
    see: http://www.reddit.com/r/programming/comments/2btrvt/gal/cj97o5f

--- solutions/complete/javascript/panzi-2/goal.js
+++ solutions/complete/javascript/panzi-2/goal.js
@@ -1,8 +1,10 @@
-// only 58 characters using JavaScript 1.8 expression closures
+// only 46 characters using JavaScript 1.8 expression closures
 // only supported by Firefox 3+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/New_in_JavaScript/1.8#Expression_closures_(Merge_into_own_page.2Fsection)
+// credits for the => syntax go to /u/inmatarian
+// http://www.reddit.com/r/programming/comments/2btrvt/gal/cj97o5f
 
-function g(a,o)a?'g'+(o||'')+a:function(a)g(a,(o||'')+'o')
+g=(a,o)=>a?'g'+(o||'')+a:(a)=>g(a,(o||'')+'o')
 
 console.log(g('al'));
 console.log(g()('al'));
