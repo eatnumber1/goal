e2111f2ff38ba8b38f57514c466a994c46bf5458
Sean McCully <sean_mccully@yahoo.com>
2014-07-27 14:34:49 -0500

    Reset List Item

--- solutions/complete/python/soln7/goal.py
+++ solutions/complete/python/soln7/goal.py
@@ -1,10 +1,14 @@
+
 def g(*a, o=[]):
     if not a:
         o.append('o')
         return g
     else:
-        return 'g' + ''.join(o) + 'al'
+        rtr_str = 'g' + ''.join(o) + 'al'
+        o.clear()
+        return rtr_str 
 
 print( g('al'))
 print( g()('al'))
 print( g()()()()()()()()()()()('al'))
+print( g('al'))
