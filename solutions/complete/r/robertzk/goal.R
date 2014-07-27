275a2e5a9a8ec1c559a3cdbd517f92e57aadbca2
RobertZK <technoguyrob@gmail.com>
2014-07-27 13:14:24 -0500

    g here

--- solutions/complete/r/robertzk/goal.R
+++ solutions/complete/r/robertzk/goal.R
@@ -1,6 +1,6 @@
 # Here's a fun solution that does not use closures
 g <- function(x) {
-  if (missing(x)) function(...) g(...)
+  if (missing(x)) g
   else {
     base <- function(z) if (is.call(z)) Recall(z[[1]]) else deparse(z)
     depth <- function(z) if (is.call(z)) 1 + Recall(z[[1]]) else 0
