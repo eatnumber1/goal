682b76ba461a5b314cad38acc7ba216257df3ee3
tomtheisen <github@tomtheisen.com>
2014-07-26 21:46:57 -0700

    added c# solution

---
+++ solutions/complete/c-sharp/soln2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program {
+    static void Main() {
+        Console.WriteLine(g("al"));
+        Console.WriteLine(g()("al"));
+        Console.WriteLine(g()()("al"));
+        Console.WriteLine(g()()()("al"));
+        Console.WriteLine("Press any key to continue.");
+        Console.ReadKey();
+    }
+
+    delegate dynamic Goal(params string[] args);
+
+    static dynamic g(string suffix = null, string prefix = "g") {
+        if (suffix == "al") return prefix + suffix;
+        return (Goal)(a => g(a.FirstOrDefault(), prefix + "o"));
+    }
+}
