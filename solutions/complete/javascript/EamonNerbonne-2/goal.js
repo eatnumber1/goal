0be76c6e1c361197976c8a3255e83dbb42a9fd80
Eamon Nerbonne <eamon@nerbonne.org>
2014-08-02 09:01:33 +0200

    Add variant when only strict-mode compatibility is necessary.
    --HG--
    extra : amend_source : 03edc6ac56624930b04aaa29ef4f43c9b28c3748

--- solutions/complete/javascript/EamonNerbonne-2/goal.js
+++ solutions/complete/javascript/EamonNerbonne-2/goal.js
@@ -3,7 +3,17 @@
 //Since each invocation has its own prefix, calls are safe to interleave
 //and g may be called multiple times. This is probably the shortest possible
 //non-ES6 solution that's safe to interleave.
+
+//valid in strict and non-strict mode: (65 or 61 chars)
 var g=(function k(s){return s?this+s:k.bind(this+'o')}).bind('g')
+
+//valid only in strict mode: (55 chars)
+function g(s,p){return p=this||'g',s?p+s:g.bind(p+'o')}
+
+
+//in your browser console, you can test strict mode as follows:
+//var g = (function(){"use strict"; return function g(s,p){p=this||'g'return s?p+s:g.bind(p+'o')}})()
+
 
 console.log(g('al'))
 console.log(g()()()('al'))
