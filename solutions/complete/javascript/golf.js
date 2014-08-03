3e77f8990bc3242210d8a9e8dda8509cb2b1f52e
Tymon Sturgeon <scryptonite.mail@gmail.com>
2014-08-02 17:46:52 -0700

    Use colors.js

--- solutions/complete/javascript/golf.js
+++ solutions/complete/javascript/golf.js
@@ -1,5 +1,6 @@
 // $ node golf.js
 var fs = require("fs");
+var colors = require("colors");
 
 console.log("");
 
@@ -14,11 +15,11 @@ function width(str, l){
 	return str;
 }
 
-console.log("%s %s gal", width("    g('al') == 'gal'", 82), width(14));
-console.log("%s %s | goal", width("    g()('al') == 'goal'", 82), width(14));
-console.log("%s %s | | gooal", width("    var goo = g()(); goo('al') == 'gooal' ", 82), width(14));
-console.log("%s %s | | | goooal ", width("    goo()('al') == 'goooal' ", 82), width(14));
-console.log("%s %s | | | | golf", width("    g()('lf') == 'golf' ", 82), width(14));
+console.log("%s %s gal", width("    g('al') == 'gal'", 82).grey, width(14));
+console.log("%s %s | goal", width("    g()('al') == 'goal'", 82).grey, width(14));
+console.log("%s %s | | gooal", width("    var goo = g()(); goo('al') == 'gooal' ", 82).grey, width(14));
+console.log("%s %s | | | goooal ", width("    goo()('al') == 'goooal' ", 82).grey, width(14));
+console.log("%s %s | | | | golf", width("    g()('lf') == 'golf' ", 82).grey, width(14));
 console.log("%s %s | | | | |", width(82), width(14));
 
 var dirs = fs.readdirSync(__dirname);
@@ -43,18 +44,18 @@ dirs.forEach(function(dir){
 				err = true;
 			}
 			try {
-				if(!err && g) _gal = g("al") == "gal" ? "\u2713" : "\u00D7";
-				if(!err && g) _goal = g()("al") == "goal" ? "\u2713" : "\u00D7";
+				if(!err && g) _gal = g("al") == "gal" ? "\u2713".green : "\u00D7".red;
+				if(!err && g) _goal = g()("al") == "goal" ? "\u2713".green : "\u00D7".red;
 				if(!err && g){
 					_goo = g()();
-					_gooal = _goo("al") == "gooal" ? "\u2713" : "\u00D7";
+					_gooal = _goo("al") == "gooal" ? "\u2713".green : "\u00D7".red;
 				}
-				if(!err && g) _goooal = _goo()("al") == "goooal" ? "\u2713" : "\u00D7";
-				if(!err && g) _golf = g()("lf") == "golf" ? "\u2713" : "\u00D7";
+				if(!err && g) _goooal = _goo()("al") == "goooal" ? "\u2713".green : "\u00D7".red;
+				if(!err && g) _golf = g()("lf") == "golf" ? "\u2713".green : "\u00D7".red;
 			} catch(e){
 				console.log(e);
 			}
-			console.log("%s %s %s %s %s %s %s  %s chars", width(golf.name || dir, 14), width(golf.src, 82),
+			console.log("%s %s %s %s %s %s %s  %s chars", width(golf.name || dir, 14).grey, width(golf.src, 82),
 				_gal,
 				_goal,
 				_gooal,
