6dec9778060be5e20776f87eb2fc41e20dbf9ab0
Russell Harmon <russ@eatnumber1.com>
2015-02-17 07:23:41 -0800

    Fix the write tests which were completely broken

--- solved/dyld/eatnumber1/test/write_test.c
+++ solved/dyld/eatnumber1/test/write_test.c
@@ -1,85 +1,54 @@
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunknown-pragmas"
-#pragma clang diagnostic ignored "-Wunused-macros"
-#define _GNU_SOURCE
-#pragma clang diagnostic pop
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "glib_include.h"
 
 #include "write_test.h"
 
-static void init_env(void) {
-	const char *ld_preload_var_name = NULL;
-	const char *lib_suffix = NULL;
-
-#if defined(__APPLE__) && defined(__MACH__)
-	g_setenv("DYLD_FORCE_FLAT_NAMESPACE", "1", TRUE);
-	ld_preload_var_name = "DYLD_INSERT_LIBRARIES";
-	lib_suffix = "dylib";
-#else
-	ld_preload_var_name = "LD_PRELOAD";
-	lib_suffix = "so";
-#endif
-
-	char *lib_path = NULL;
-	asprintf(&lib_path, "%s/goal.%s", getcwd(NULL, 0), lib_suffix);
-	g_setenv(ld_preload_var_name, lib_path, TRUE);
-	free(lib_path);
-}
-
-static bool start_subprocess_with_env(const char *path) {
-	if (g_test_subprocess()) {
-		init_env();
+static void start_subproc() {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunknown-pragmas"
 #pragma clang diagnostic ignored "-Wassign-enum"
-		GTestSubprocessFlags flags =
-			G_TEST_SUBPROCESS_INHERIT_STDIN |
-			G_TEST_SUBPROCESS_INHERIT_STDOUT |
-			G_TEST_SUBPROCESS_INHERIT_STDERR;
+	g_test_trap_subprocess(NULL, 0, 0);
 #pragma clang diagnostic pop
-		g_test_trap_subprocess(path, 0, flags);
-		g_assert(g_test_trap_has_passed());
-		return true;
+}
+
+static void write_str(const char *str) {
+	size_t len = strlen(str);
+	ssize_t written = write(STDOUT_FILENO, str, len);
+	if (written == -1) {
+		perror("write");
+		exit(EXIT_FAILURE);
 	}
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunknown-pragmas"
-#pragma clang diagnostic ignored "-Wassign-enum"
-	g_test_trap_subprocess(path, 0, 0);
-#pragma clang diagnostic pop
-
-	return false;
-}
-
-static void without_goal_subproc(void) {
-	printf("hello");
+	g_assert_cmpuint((size_t) written, ==, len);
 }
 
 static void without_goal(void) {
-	if (start_subprocess_with_env("/dyld/write/without_goal/subprocess")) return;
+	if (g_test_subprocess()) {
+		write_str("hello");
+		return;
+	}
+
+	start_subproc();
 	g_test_trap_assert_passed();
 	g_test_trap_assert_stdout("hello");
 }
 
-static void single_goal_subproc(void) {
-	printf("g()('al')");
-}
-
 static void single_goal(void) {
-	if (start_subprocess_with_env("/dyld/write/single_goal/subprocess")) return;
+	if (g_test_subprocess()) {
+		write_str("g()('al')");
+		return;
+	}
+
+	start_subproc();
 	g_test_trap_assert_passed();
-	g_test_trap_assert_stdout("g()('al')");
+	g_test_trap_assert_stdout("goal");
 }
 
 void write_test_register() {
-	g_test_add_func("/dyld/write/without_goal", without_goal);
-	g_test_add_func("/dyld/write/without_goal/subprocess", without_goal_subproc);
-	g_test_add_func("/dyld/write/single_goal", single_goal);
-	g_test_add_func("/dyld/write/single_goal/subprocess", single_goal_subproc);
+	g_test_add_func("/write/without_goal", without_goal);
+	g_test_add_func("/write/single_goal", single_goal);
 }
